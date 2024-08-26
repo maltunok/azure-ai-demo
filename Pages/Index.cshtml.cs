133f980282f2892ff52f742e640f85c3b718d71d
Mustafa Altunok <mustafa.altunok@devbase.com.tr>
2024-08-26 20:22:46 +0300

    initial demo

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Net.WebRequestMethods;
+
+namespace StoryCreator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Question { get; set; } = String.Empty;
+        public string ResponseContent { get; set; } = String.Empty;
+
+        public void OnPost()
+        {
+            // Configure OpenAI client
+            string openAIEndpoint = "https://your-openai-service-name.openai.azure.com/";
+            string openAIKey = "your-openai-service-key";
+            string openAIDeploymentName = "dyour-openai-deployment-name";
+
+            OpenAIClient client = new(new Uri(openAIEndpoint), new AzureKeyCredential(openAIKey));
+
+            // Configure search service
+            string searchEndpoint = "https://your-azure-search-service-name.search.windows.net";
+            string searchKey = "your-azure-search-service-key";
+            string searchIndex = "your-serach-index-name";
+
+            AzureCognitiveSearchChatExtensionConfiguration demoConfig = new()
+            {
+                SearchEndpoint = new Uri(searchEndpoint),
+                Authentication = new OnYourDataApiKeyAuthenticationOptions(searchKey),
+                IndexName = searchIndex
+            };
+
+            // Set up the AI chat query/completion
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages = { new ChatRequestUserMessage(Question) },
+                AzureExtensionsOptions = new AzureChatExtensionsOptions
+                {
+                    Extensions = { demoConfig }
+                },
+                DeploymentName = openAIDeploymentName
+            };
+
+            // Send request to Azure OpenAI model
+            ChatCompletions chatCompletionsResponse = client.GetChatCompletions(chatCompletionsOptions);
+
+            ResponseContent = chatCompletionsResponse.Choices[0].Message.Content;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+1. Create Azure Open AI
+2. Create Azure AI Search Service
+3. Create Storage Account, a blob container in it and upload a test pdf
+4. Create deployment in Azure OpenAI Studio with model gpt-35-turbo (model version 0301)
+5. From Chat blade Add your data source, select created blob container, Azure AI Search Service, and enter index name. 
+
+6. Populate Azure Open AI endpoint, key and deployment name to create Open AI client
+7. Populate Azure AI Search endpoint, key and index name to configure search service
